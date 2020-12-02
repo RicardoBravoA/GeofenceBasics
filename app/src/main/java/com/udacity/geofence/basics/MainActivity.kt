e8afb34f31f866c7cdd19e17b94207566102fdc7
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-02 00:16:03 -0500

    refactor: Remove fragment

--- app/src/main/java/com/udacity/geofence/basics/MainActivity.kt
+++ app/src/main/java/com/udacity/geofence/basics/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.udacity.geofence.basics
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.gms.location.GeofencingClient
+import com.udacity.geofence.basics.databinding.ActivityMainBinding
+import com.udacity.geofence.basics.receiver.GeofenceBroadcastReceiver
+import com.udacity.geofence.basics.util.Constant
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var geofencingClient: GeofencingClient
+    private lateinit var mainViewModel: MainViewModel
+
+    private val runningQOrLater =
+        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+
+    // A PendingIntent for the Broadcast Receiver that handles geofence transitions.
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
+        intent.action = Constant.ACTION_GEOFENCE_EVENT
+        // Use FLAG_UPDATE_CURRENT so that you get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,4 +50,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
